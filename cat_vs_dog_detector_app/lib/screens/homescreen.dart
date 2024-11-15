4b94ed1fe6329fd3a89fc77364015a1a81b56f28
Slozzyondul <solomonondula@gmail.com>
2024-11-15 20:00:19 +0300

    few UI updates

--- cat_vs_dog_detector_app/lib/screens/homescreen.dart
+++ cat_vs_dog_detector_app/lib/screens/homescreen.dart
@@ -11,16 +11,38 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold( 
+      child: Scaffold(
         appBar: AppBar(),
         body: Container(
+          padding: EdgeInsetsDirectional.all(2),
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [Colors.black, Colors.yellow],
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
             ),
-            
+          ),
+          child: const Column(
+            children: <Widget>[
+              SizedBox(height: 50),
+              Text(
+                'What you need',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                'Is it a cat or a dog?',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SizedBox(height: 5),
+            ],
           ),
         ),
       ),
