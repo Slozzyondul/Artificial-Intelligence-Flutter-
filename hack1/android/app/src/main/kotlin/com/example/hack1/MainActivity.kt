d1ff589fab8fe3a382489d8277a525d3049fab21
Slozzyondul <solomonondula@gmail.com>
2024-11-28 14:59:13 +0300

    removing uneeded files and finaliying text extractor from image

--- hack1/android/app/src/main/kotlin/com/example/hack1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.hack1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- hack3-flask server - Image-to-text convertor app/android/app/src/main/kotlin/com/example/hack3/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.hack3
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
